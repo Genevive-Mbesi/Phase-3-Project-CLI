d1f220f12951d4fbff982ab0a4f5b7c5fc25ea5d
Genevive-Mbesi <genevive.opondo@student.moringaschool.com>
2023-09-08 15:27:38 +0300

    ff

---
+++ cli.py
@@ -0,0 +1,58 @@
+import click
+from inventory.models.model import Category, Product, Supplier, engine, session
+
+
+@click.group()
+def cli():
+    pass
+
+# No need to create the tables again; they are created in model.py
+# Base.metadata.create_all(engine)
+
+@cli.command()
+@click.option('--name', prompt='Category Name', help='Name of the category')
+def create_category(name):
+    category = Category.create(name)
+    click.echo(f"Category '{category.name}' created with ID: {category.id}")
+
+@cli.command()
+@click.option('--name', prompt='Supplier Name', help='Name of the supplier')
+def create_supplier(name):
+    supplier = Supplier.create(name)
+    click.echo(f"Supplier '{supplier.name}' created with ID: {supplier.id}")
+
+
+
+@cli.command()
+@click.option('--name', prompt='Product Name', help='Name of the product')
+@click.option('--description', prompt='Product Description', help='Description of the product')
+@click.option('--quantity', prompt='Quantity', default=0, type=int, help='Quantity of the product')
+@click.option('--price', prompt='Price', type=int, help='Price of the product')
+@click.option('--category_id', prompt='Category ID', type=int, help='ID of the category')
+@click.option('--supplier_id', prompt='Supplier ID', type=int, help='ID of the supplier')
+def create_product(name, description, quantity, price, category_id, supplier_id):
+    category = Category.get_by_id(category_id)
+    supplier = Supplier.get_by_id(supplier_id)
+    product = Product.create(name, description, quantity, price, category, supplier)
+    click.echo(f"Product '{product.name}' created with ID: {product.id}")
+
+
+@cli.command()
+def list_categories():
+    print("Inside list_categories function")  # Debugging line
+    categories = Category.list_categories()
+    click.echo("Categories:")
+    for category in categories:
+        click.echo(f"ID: {category.id}, Name: {category.name}")
+
+
+@cli.command()
+def list_products():
+    products = Product.list_products()
+    click.echo("Products:")
+    for product in products:
+        click.echo(f"ID: {product.id}, Name: {product.name}, Price: {product.price}")
+
+
+if __name__ == '__main__':
+    cli()
--- inventory/models/model.py
+++ inventory/models/model.py
@@ -1,8 +1,24 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+import os
+import sys
+
+Project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    # Define your database connection URL
+import os
+
+# Define the absolute path to the database file
+db_file = os.path.join(os.path.dirname(__file__), 'inventory.db')
+
+# Use the absolute path to create the engine
+engine = create_engine(f'sqlite:///{db_file}')
+
+Session = sessionmaker(bind=engine)
+session = Session()
 Base = declarative_base()
+
 
 class Category(Base):
     __tablename__ = 'categories'
@@ -10,6 +26,24 @@ class Category(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     products = relationship('Product', back_populates='category')
+
+    @classmethod
+    def create(cls, name):
+        category = cls(name=name)
+        session.add(category)
+        session.commit()
+        return category
+
+    @classmethod
+    def get_by_id(cls, category_id):
+        return session.query(cls).filter_by(id=category_id).first()
+
+    @classmethod
+    def list_categories(cls):
+        return session.query(cls).all()
+
+    def __str__(self):
+        return self.name
 
 
 class Product(Base):
@@ -27,6 +61,26 @@ class Product(Base):
     supplier_id = Column(Integer, ForeignKey('suppliers.id'))
     supplier = relationship('Supplier', back_populates='products')  
 
+
+
+    @classmethod
+    def create(cls, name, description, quantity, price, category, supplier):
+        product = cls(name=name, description=description, quantity=quantity, price=price, category=category, supplier=supplier)
+        session.add(product)
+        session.commit()
+        return product
+
+    @classmethod
+    def get_by_id(cls, product_id):
+        return session.query(cls).filter_by(id=product_id).first()
+
+    @classmethod
+    def list_products(cls):
+        return session.query(cls).all()
+
+    def __str__(self):
+        return self.name
+
 class Supplier(Base):
     __tablename__ = 'suppliers'
     
@@ -35,3 +89,17 @@ class Supplier(Base):
     
     #  relationship with Product
     products = relationship('Product', back_populates='supplier')  
+
+    @classmethod
+    def create(cls, name):
+        supplier = cls(name=name)
+        session.add(supplier)
+        session.commit()
+        return supplier
+
+    @classmethod
+    def get_by_id(cls, supplier_id):
+        return session.query(cls).filter_by(id=supplier_id).first()
+
+    def __str__(self):
+        return self.name
